57f5ad807aaa4c6f2301385697c19003d03cdaaf
getishe <getahune.alemayhu@gmail.com>
2025-02-24 16:10:44 +0300

    Refactor components and styles for improved layout and readability

--- alx-react-app/src/components/MainContent.jsx
+++ alx-react-app/src/components/MainContent.jsx
@@ -1,9 +1,18 @@
 function MainContent() {
-    return (
-        <main>
-            <p>I love to visit New York, Paris, and Tokyo.</p>
-        </main>
-    );
+  return (
+    <main style={{ border: "2px solid grey", padding: "2px", margin: "10px" }}>
+      <p
+        style={{
+          backgroundColor: "whitesmoke",
+          fontSize: "22px",
+          color: "black",
+          textAlign: "center",
+        }}
+      >
+        I love to visit New York, Paris, and Tokyo.
+      </p>
+    </main>
+  );
 }
 
 export default MainContent;
