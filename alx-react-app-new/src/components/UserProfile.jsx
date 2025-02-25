bd6792d4e4cc63259fe73cc7fe9f3f18b8b4c69a
getishe <getahune.alemayhu@gmail.com>
2025-02-25 15:15:57 +0300

    modified some with styles

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 const UserProfile = (props) => {
   return (
-    <div style={{ border: "1px solid black", padding: "10px", margin: "10px" }}>
+    <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
       <h2 style={{ color: "blue" }}>{props.name}</h2>
       <p>
         Age: <span style={{ fontWeight: "bold" }}> {props.age}</span>
