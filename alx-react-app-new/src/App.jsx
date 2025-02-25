c41068abd308685117cc4b5af0e9e1fad7dd824f
getishe <getahune.alemayhu@gmail.com>
2025-02-25 15:50:38 +0300

    set a increment and decrement

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 function App() {
   // const [count, setCount] = useState(0);
 
@@ -16,6 +17,9 @@ function App() {
       <div>
         <MainContent />
         <UserProfile name="Alice" age="25" bio="I love cats" />
+      </div>
+      <div>
+        <Counter />
       </div>
       <div>
         <Footer />
