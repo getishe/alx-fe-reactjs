47b197d2b618e1812a5e60a7f2c14b15e2c7cb6b
getishe <getahune.alemayhu@gmail.com>
2025-02-25 13:52:43 +0300

    Add initial React components and Vite configuration

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,28 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+// import { useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
-
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <Header />
-        <MainContent />
-      </div>
-      <div>
         <WelcomeMessage />
+        <Header />
       </div>
       <div>
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-        <UserProfile name="Bob" age="30" bio="Enjoys cooking and traveling" />
+        <MainContent />
+        <UserProfile name="Alice" age="25" bio="I love cats" />
       </div>
       <div>
         <Footer />
