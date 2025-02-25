acb12d29c538432dd20b5aad7fc12120a2d97bca
getishe <getahune.alemayhu@gmail.com>
2025-02-25 15:50:38 +0300

    set a increment and decrement

---
+++ alx-react-app-new/src/components/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <button onClick={() => setCount(1)}>Reest</button>
+    </div>
+  );
+}
+
+export default Counter;
