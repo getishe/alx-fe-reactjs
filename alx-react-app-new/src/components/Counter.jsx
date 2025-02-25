e204dee42aef311983a71e9f2385677592785d8f
getishe <getahune.alemayhu@gmail.com>
2025-02-25 15:58:03 +0300

    some changes with reset button

--- alx-react-app-new/src/components/Counter.jsx
+++ alx-react-app-new/src/components/Counter.jsx
@@ -7,7 +7,7 @@ function Counter() {
       <p>count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
-      <button onClick={() => setCount(0)}>Rest</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   );
 }
