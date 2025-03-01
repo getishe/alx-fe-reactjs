613cdb63bf471b2ab71c896e7342ed5173b15b18
getishe <getahune.alemayhu@gmail.com>
2025-03-01 09:34:05 +0300

    update Navbar component links to include trailing spaces for consistency

--- my-company/src/Components/Navbar.jsx
+++ my-company/src/Components/Navbar.jsx
@@ -6,16 +6,16 @@ function Navbar() {
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">Home </Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about">About </Link>
         </li>
         <li>
           <Link to="/services">Services </Link>
         </li>
         <li>
-          <Link to="/contact">Contact</Link>
+          <Link to="/contact">Contact </Link>
         </li>
       </ul>
     </nav>
