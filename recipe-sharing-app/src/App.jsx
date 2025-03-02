4cf629aae160aa0ee8cd1ca42ac806fa013e36f1
getishe <getahune.alemayhu@gmail.com>
2025-03-02 22:23:24 +0300

    feat(routes): implement routing for recipe details and editing, add delete functionality

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,18 @@
+// src/App.jsx
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetails from "./components/RecipeDetails";
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={RecipeList} />
+        <Route path="/add" component={AddRecipeForm} />
+        <Route path="/recipe/:id" component={RecipeDetails} />
+      </Switch>
+    </Router>
   );
 };
 
