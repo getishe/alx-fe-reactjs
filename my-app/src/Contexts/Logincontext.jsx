28dab1c7308751de490c7aae47e8db0b36fe4210
getishe <getahune.alemayhu@gmail.com>
2025-03-05 09:16:40 +0300

    feat: initialize React app with Vite setup, add context for login management, and create basic components

---
+++ my-app/src/Contexts/Logincontext.jsx
@@ -0,0 +1,6 @@
+import { createContext } from "react";
+
+// Create a context for login information. The initial value is null.
+const LoginContext = createContext(null);
+
+export default LoginContext;
