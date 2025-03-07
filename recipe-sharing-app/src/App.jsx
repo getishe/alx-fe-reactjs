4aa7dd98c8f1fbe5bbe34539137353dd4e04d481
getishe <getahune.alemayhu@gmail.com>
2025-03-07 18:43:16 +0300

    importing router dom into app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
@@ -7,11 +7,11 @@ import RecipeDetails from "./components/RecipeDetails";
 const App = () => {
   return (
     <Router>
-      <Switch>
+      <Routes>
         <Route path="/" exact component={RecipeList} />
         <Route path="/add" component={AddRecipeForm} />
         <Route path="/recipe/:id" component={RecipeDetails} />
-      </Switch>
+      </Routes>
     </Router>
   );
 };
