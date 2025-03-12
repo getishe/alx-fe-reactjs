89d94dd8370ced74628609d984d3ac7bdec819c7
getishe <getahune.alemayhu@gmail.com>
2025-03-12 18:54:31 +0300

    Update App.jsx

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+// import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
+function App() {
+  return (
+    <>
+      <h1>Form Handling with React</h1>
+      <FormikForm />
+      <h1>head</h1>
+
+      <h1>head</h1>
+    </>
+  );
+}
+
+export default App;
