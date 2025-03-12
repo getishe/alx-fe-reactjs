966fd95e87d3829dbfd7d3a89849f4c09acc0c1e
getishe <getahune.alemayhu@gmail.com>
2025-03-12 18:55:08 +0300

    Error: Failed to get git output.

---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { useState } from "react";
+
+/**
+ * Defines the main registration form component for the application.
+ */
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({ name: "", email: "", password: "" });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    // setFormData({ ...formData, [name]: value });
+
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+    console.log(formData);
+  };
+
+  const validationForm = () => {
+    const newErrors = {};
+    let isValid = true;
+
+    if (!formData.name) {
+      newErrors.name = "Name is required";
+      isValid = false;
+    }
+
+    if (!formData.email) {
+      newErrors.email = "Email is required";
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      newErrors.email = "Invalid email format";
+      isValid = false;
+    }
+    if (!formData.password) {
+      newErrors.password = "Password is required";
+      isValid = false;
+    } else if (formData.password.length < 8) {
+      newErrors.password = "Password must be at least 8 characters long";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (validationForm()) {
+      alert("Form submitted successfully!");
+      // Perform form submission or further processing
+      console.log("Form submitted:", formData);
+
+      // Reset the form after submission
+      setFormData({ name: "", email: "", password: "" });
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        className="name"
+        type="text"
+        name="name"
+        placeholder="name"
+        value={formData.name}
+        onChange={handleChange}
+      />
+      {errors.name && <div>{errors.name}</div>}
+
+      <input
+        className="email"
+        type="email"
+        name="email"
+        placeholder="email"
+        value={formData.email}
+        // style={{ color: "red" }}
+        onChange={handleChange}
+      />
+      {errors.email && <div>{errors.email}</div>}
+      <input
+        className="password"
+        type="password"
+        name="password"
+        placeholder="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      {errors.password && <div>{errors.password}</div>}
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default RegistrationForm;
