d39becb578a2f0c8c2a9019a30f7dce931034c95
getishe <getahune.alemayhu@gmail.com>
2025-03-13 23:10:58 +0300

    feat: Implement basic profile page with nested routes

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
-function Profile() {
-  return <div>Profile</div>;
-}
+const Profile = () => {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <nav>
+        <Link to="details">Profile Details</Link>
+        <Link to="settings">Profile Settings</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
 
 export default Profile;
