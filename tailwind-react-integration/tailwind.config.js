05abd687267ab3c745a7eac05432490397f21c25
getishe <getahune.alemayhu@gmail.com>
2025-03-18 16:35:01 +0300

    contain: ["purge", "darkMode", "variants"]

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,12 +1,25 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+//   theme: {
+//     extend: {
+//       // You can add custom styling here, such as colors, fonts, etc.
+//     },
+//   },
+//   plugins: [
+//     // You can add Tailwind plugins here if needed.
+//   ],
+// };
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Purge unused styles
+  darkMode: "class", // You can use "media" or "class"
   theme: {
-    extend: {
-      // You can add custom styling here, such as colors, fonts, etc.
-    },
+    extend: {}, // Add custom styling here
   },
-  plugins: [
-    // You can add Tailwind plugins here if needed.
-  ],
+  variants: {
+    extend: {}, // Extend variant settings if needed
+  },
+  plugins: [], // Add plugins here if required
 };
