dafd5b05ce0a3e4884ae626c3f0bdc06f9051b06
getishe <getahune.alemayhu@gmail.com>
2025-03-18 14:48:50 +0300

    change a path

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Updated for unused styles removal
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // You can keep this or modify based on your project needs
   theme: {
     extend: {},
