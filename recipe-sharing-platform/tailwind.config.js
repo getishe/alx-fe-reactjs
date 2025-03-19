758ca22f9d1161e5f7a11bd9589e786f971ba6d0
getishe <getahune.alemayhu@gmail.com>
2025-03-19 17:42:37 +0300

    changing purge into content

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,19 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Purge unused styles
+//   darkMode: "class", // You can use "media" or "class"
+//   theme: {
+//     extend: {}, // Add custom styling here
+//   },
+//   variants: {
+//     extend: {}, // Extend variant settings if needed
+//   },
+//   plugins: [], // Add plugins here if required
+// };
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"], // Purge unused styles
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: "class", // You can use "media" or "class"
   theme: {
     extend: {}, // Add custom styling here
