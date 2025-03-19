f0ca55ab77c931c045072b5d9d1b5b05fba1e05e
getishe <getahune.alemayhu@gmail.com>
2025-03-19 16:40:49 +0300

    Adding Advanced Interactivity and Transitions with Tailwind CSS

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm">
+    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 sm:max-w-xs md:max-w-sm hover:shadow-xl">
       <img
-        src="https://via.placeholder.com/150"
+        src="https://images.mediago.io/ML/423feb9431560c453ac521992cea73b1__scv1__622x368.png"
         alt="User"
-        className="rounded-full w-36 h-36 mx-auto border-2 border-blue-500 border-solid sm:w-24 sm:h-24 md:w-36 md:h-36"
+        className="rounded-full w-28 h-28 mx-auto border-2 border-blue-500 border-solid sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl">
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-400">
         John Doe
       </h1>
-      <p className="text-gray-600 text-base sm:text-sm md:text-base">
+      <p className="text-gray-600 text-base sm:text-sm md:text-base hover:text-gray-900">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
