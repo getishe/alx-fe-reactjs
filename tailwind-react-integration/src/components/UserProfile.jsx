50e30b5f478b8de4e97be5b654d158026d87d205
getishe <getahune.alemayhu@gmail.com>
2025-03-19 16:42:50 +0300

    have effect change's

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -8,10 +8,10 @@ function UserProfile() {
         alt="User"
         className="rounded-full w-28 h-28 mx-auto border-2 border-blue-500 border-solid sm:w-24 sm:h-24 md:w-36 md:h-36 hover:scale-110 transition-transform duration-300 ease-in-out"
       />
-      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-400">
+      <h1 className="text-xl text-blue-800 my-4 sm:text-lg md:text-xl hover:text-blue-500">
         John Doe
       </h1>
-      <p className="text-gray-600 text-base sm:text-sm md:text-base hover:text-gray-900">
+      <p className="text-gray-600 text-base sm:text-sm md:text-base hover:text-gray-500">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
