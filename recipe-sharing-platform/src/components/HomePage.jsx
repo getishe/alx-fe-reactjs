8e5dc6a2dd22781eceb3d866c0245cd1171d5c2b
getishe <getahune.alemayhu@gmail.com>
2025-03-20 22:28:48 +0300

    Responsive Layout Design

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -46,7 +46,7 @@ const HomePage = () => {
   return (
     <div className="container mx-auto p-4 hover:shadow-lg duration-75 ">
       <h1 className="text-3xl font-bold mb-4">Recipe Sharing Platform</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid grid-flow-col-2 lg:grid-cols-3 gap-4">
         {recipes.map((recipe) => (
           <div key={recipe.id} className="bg-white rounded-lg shadow-lg p-4">
             <img
