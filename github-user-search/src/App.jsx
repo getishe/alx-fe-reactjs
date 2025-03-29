60f9d00af68a6517520153e91cc81bfc5fa688bc
getishe <getahune.alemayhu@gmail.com>
2025-03-29 17:54:19 +0300

    add gitignore

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Search from "./components/Search";
+import "./App.css";
+
+const App = () => {
+  return (
+    <main className="app">
+      <Search />
+    </main>
+  );
+};
+
+export default App;
