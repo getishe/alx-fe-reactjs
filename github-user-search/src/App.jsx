113c92f2a9042e73092229f61ae0394dc8f9a1f5
getishe <getahune.alemayhu@gmail.com>
2025-03-29 14:29:45 +0300

    app edited

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,13 +1,14 @@
-import Search from "./components/Search"
-import './App.css'
+import Search from "./components/Search";
+import "./App.css";
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
-  <div>
-    <Search />
-  </div>
-  )
+    <div>
+      <Search />
+      {/* search  */}
+    </div>
+  );
 }
 
-export default App
+export default App;
