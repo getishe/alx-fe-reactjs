d6dce3b0ded3ee43d2c792c02e41435dd8fd53da
getishe <getahune.alemayhu@gmail.com>
2025-03-30 09:11:03 +0300

    Home navigation

--- github-user-search/src/components/Home.jsx
+++ github-user-search/src/components/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
-
+import { Navigate, Link } from "react-router-dom";
 function Home() {
-  return <div>Home</div>;
+  const Navigate = () => {
+    return <Navigate to="/search" />;
+  };
+  return (
+    <div>
+      {" "}
+      Home <Link to="/search">Search</Link>
+    </div>
+  );
 }
 
 export default Home;
